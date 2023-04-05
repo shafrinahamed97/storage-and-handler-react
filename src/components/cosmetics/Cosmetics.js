50a7ec90b4312f7bbe8bef0f5c267ce06e77cf66
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-04-05 12:01:35 +0600

    first commit

---
+++ src/components/cosmetics/Cosmetics.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cosmetics.css';
+
+const Cosmetics = () => {
+    return (
+        <div>
+            <h1>Welcome to My Cosmetics store</h1>
+        </div>
+    );
+};
+
+export default Cosmetics;
